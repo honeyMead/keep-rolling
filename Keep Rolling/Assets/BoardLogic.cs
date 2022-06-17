3159383908aecbb111d4bb460844e80e01cbb572
honeyMead <70470522+honeyMead@users.noreply.github.com>
2022-06-17 20:07:25 +0200

    added board, ball and controls

---
+++ Keep Rolling/Assets/BoardLogic.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+public class BoardLogic : MonoBehaviour
+{
+    public float MaxRotationEuler;
+    public float RotationSpeed;
+
+    private Vector3 currentEulerAngles;
+
+    void Update()
+    {
+        RotateBoard();
+    }
+
+    private void RotateBoard()
+    {
+        var mousePosPercent = GetPercentageOfMousePosition();
+        var xPercent = mousePosPercent.Item1;
+        var zPercent = mousePosPercent.Item2;
+
+        var targetX = xPercent * MaxRotationEuler;
+        var targetZ = zPercent * MaxRotationEuler;
+        currentEulerAngles += RotationSpeed * Time.deltaTime * new Vector3(targetX, 0f, targetZ);
+
+        currentEulerAngles.x = LimitAngleToMaximum(currentEulerAngles.x);
+        currentEulerAngles.z = LimitAngleToMaximum(currentEulerAngles.z);
+
+        transform.eulerAngles = currentEulerAngles;
+    }
+
+    private float LimitAngleToMaximum(float vectorComponent)
+    {
+        float result = vectorComponent;
+        if (Mathf.Abs(vectorComponent) > MaxRotationEuler)
+        {
+            result = MaxRotationEuler * Mathf.Sign(vectorComponent);
+        }
+        return result;
+    }
+
+    private static Tuple<float, float> GetPercentageOfMousePosition()
+    {
+        var screenPoint = Input.mousePosition;
+        var mousePosition = Camera.main.ScreenToViewportPoint(screenPoint);
+        var halfOfViewPort = 0.5f;
+
+        var x = -(halfOfViewPort - mousePosition.y);
+        var z = halfOfViewPort - mousePosition.x;
+
+        var maxDisplayPos = 0.5f;
+        var xPercent = x / maxDisplayPos;
+        var zPercent = z / maxDisplayPos;
+
+        return Tuple.Create(xPercent, zPercent);
+    }
+}
